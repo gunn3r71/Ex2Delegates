cd5749cbf19a0b2ffe6d0b020b27a8c1d6d62fff
Lucas <lucasgunner3@gmail.com>
2021-11-23 21:05:09 -0300

    Adicionar arquivos de projeto.

---
+++ Ex2Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex2Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
